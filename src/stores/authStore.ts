f57b8c2f2f69e65f78dfdfd9621dde54ba73f8c8
jiyeonyooo <ji04230423@gmail.com>
2026-01-22 12:58:08 +0900

    feat: zustand 기반 accessToken 스토어 추가

---
+++ src/stores/authStore.ts
@@ -0,0 +1,17 @@
+import { create } from "zustand";
+
+type AuthState = {
+  accessToken: string | null;
+  setAccessToken: (token: string | null) => void;
+  clear: () => void;
+};
+
+export const useAuthStore = create<AuthState>((set) => ({
+  accessToken: null,
+  setAccessToken: (accessToken) => set({ accessToken: accessToken }),
+  clear: () => set({ accessToken: null }),
+}));
+
+export const authStore = {
+  getAccessToken: () => useAuthStore.getState().accessToken,
+};
