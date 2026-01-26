e3e53b79d00730e05591d6fb00ca80985ffe20b1
jiyeonyooo <ji04230423@gmail.com>
2026-01-26 21:17:37 +0900

    chore: next.config에 외부 이미지 remotePatterns 등록

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "search1.kakaocdn.net",
+        pathname: "/thumb/**",
+      },
+      {
+        protocol: "https",
+        hostname: "t1.daumcdn.net",
+        pathname: "/**",
+      },
+      {
+        protocol: "http",
+        hostname: "t1.daumcdn.net",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
