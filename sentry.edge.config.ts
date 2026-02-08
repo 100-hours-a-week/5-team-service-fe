a400de5d7926196d4d683080aeca8f1a38afadb3
jiyeonyooo <ji04230423@gmail.com>
2026-02-08 20:38:46 +0900

    chore: Sentry 초기 설정 파일 추가

---
+++ sentry.edge.config.ts
@@ -0,0 +1,10 @@
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  environment: process.env.NEXT_PUBLIC_SENTRY_ENV,
+
+  tracesSampleRate: 1,
+  enableLogs: true,
+  sendDefaultPii: true,
+});
