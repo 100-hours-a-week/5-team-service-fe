9e7ce8a9d3ba0f66d372eb81c92ecc666ce0bde9
jiyeonyooo <ji04230423@gmail.com>
2026-02-08 20:29:24 +0900

    chore: Sentry 소스맵 업로드 설정 추가

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+import { withSentryConfig } from "@sentry/nextjs";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -32,4 +33,18 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withSentryConfig(nextConfig, {
+  org: "doktori",
+  project: "doktori-fe",
+
+  silent: !process.env.CI,
+  widenClientFileUpload: true,
+
+  webpack: {
+    automaticVercelMonitors: true,
+
+    treeshake: {
+      removeDebugLogging: true,
+    },
+  },
+});
