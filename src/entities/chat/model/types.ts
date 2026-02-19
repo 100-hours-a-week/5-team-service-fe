59297833a15cc091d38406d4cf04e393c8c7052d
jiyeonyooo <ji04230423@gmail.com>
2026-02-19 21:37:59 +0900

    feat: 채팅방 엔티티 타입 정의 추가

---
+++ src/entities/chat/model/types.ts
@@ -0,0 +1,10 @@
+export type Chat = {
+  roomId: number;
+  topic: string;
+  description: string;
+  capacity: number;
+  currentMemberCount: number;
+  bookTitle: string;
+  bookAuthors: string;
+  bookThumbnailUrl: string;
+};
