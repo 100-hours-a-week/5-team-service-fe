2df799cae9dba429e8b8ad941ca5c077091eecb7
jiyeonyooo <ji04230423@gmail.com>
2026-02-27 20:22:35 +0900

    feat: 채팅 결과 엔트리 페이지 구현

---
+++ src/views/chat-result/ui/Page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import PageHeader from "@/components/layout/PageHeader";
+import ChatResult from "@/features/chat-result/ui/ChatResult";
+
+export default function ChatResultPage({ roomId }: { roomId: number }) {
+  return (
+    <div className="flex h-dvh min-h-0 flex-col bg-white">
+      <PageHeader title="투표 및 결과 조회" showBackButton={false} />
+      <ChatResult roomId={roomId} />
+    </div>
+  );
+}
