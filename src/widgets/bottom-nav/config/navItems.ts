170b9a064178873cdbe321ec83f662b7c0481500
jiyeonyooo <ji04230423@gmail.com>
2026-03-11 10:54:21 +0900

    chore: 하단 탭바 북마크 아이템 추가

--- src/widgets/bottom-nav/config/navItems.ts
+++ src/widgets/bottom-nav/config/navItems.ts
@@ -3,6 +3,7 @@ import {
   ChatBubbleLeftEllipsisIcon,
   UserCircleIcon,
   BookOpenIcon,
+  BookmarkIcon,
 } from "@heroicons/react/24/outline";
 import { ComponentType } from "react";
 import { FireIcon, ChatBubbleOvalLeftEllipsisIcon } from "@heroicons/react/24/solid";
@@ -15,8 +16,9 @@ type BottomNavItem = {
 };
 
 export const bottomNavItems: BottomNavItem[] = [
-  { key: "home", label: "홈", href: "/", Icon: HomeIcon },
+  { key: "bookmark", label: "관심 모임", href: "/bookmark", Icon: BookmarkIcon },
   { key: "my-meeting", label: "나의 모임", href: "/my-meeting", Icon: BookOpenIcon },
+  { key: "home", label: "홈", href: "/", Icon: HomeIcon },
   { key: "chat", label: "채팅 토론", href: "/chats", Icon: ChatBubbleLeftEllipsisIcon },
   { key: "my", label: "마이페이지", href: "/my", Icon: UserCircleIcon },
 ] as const;
